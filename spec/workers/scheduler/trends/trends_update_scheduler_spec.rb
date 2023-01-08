a84bb3ad41f268390008e4995a4bea75d291fabb
Shalom Dubinsky <smdubinsky@gmail.com>
2023-01-08 14:53:27 +0200

    max score tests

--- spec/workers/scheduler/trends/trends_update_scheduler_spec.rb
+++ spec/workers/scheduler/trends/trends_update_scheduler_spec.rb
@@ -26,7 +26,7 @@ describe Scheduler::Trends::TrendsUpdateScheduler do
            attributedTo: ActivityPub::TagManager.instance.uri_for(account),
            replies_count: 1,
            favourites_count: 1,
-           reblogs_count: 1
+           reblogs_count: 1,
           }
         end
       end
@@ -34,6 +34,24 @@ describe Scheduler::Trends::TrendsUpdateScheduler do
 
       it 'creates the statuses' do
         expect { subject.get_statuses(url) }.to change(Status, :count).by(20)
+      end
+    end
+  end
+
+  describe '#calculate_max_score' do
+    context 'when history is too short' do
+      let(:history) { [] }
+
+      it 'returns 0' do
+        expect(subject.calculate_max_score(history)).to eq(0)
+      end
+    end
+
+    context 'when history exists' do
+      let(:history) { [{ 'accounts' => 4 }, { 'accounts' => 5 }] }
+
+      it 'calculates the max score' do
+        expect(subject.calculate_max_score(history)).to eq(0.2)
       end
     end
   end
