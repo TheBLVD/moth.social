c2b074078ed25bf24d5159539cea3c9b2a32447d
Felipe Lima <felipe.lima@gmail.com>
2023-01-17 07:56:55 -0500

    fix: [MAM-239] Fix `FollowRecommendations` suggesting users already being followed (#79)
    * fix: Fix FollowRecommendations suggesting users already being followed
    
    * comment nits
    
    * Filter returned list of follow recommendations to remove existing follows
    
    * revert this change

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -72,9 +72,12 @@ class Follow < ApplicationRecord
   end
 
   def refresh_follow_recommendations_cache
+    # We'll currently only prefetch follow recommendations for moth.social users.
     return unless account.local?
 
-    FollowRecommendationsRefreshWorker.perform_async(account.local_username_and_domain)
+    handle = account.local_username_and_domain
+    limit = FollowRecommendations::DEFAULT_FOLLOW_LIMIT
+    FollowRecommendationsRefreshWorker.perform_async(handle, limit)
   end
 
   def decrement_cache_counters
