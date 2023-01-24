dceb65ecb33e5de6ae89883b693fcbde76173bbe
Shalom Dubinsky <smdubinsky@gmail.com>
2023-01-24 14:30:25 +0200

    explictly set datadog service names

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -4,5 +4,9 @@ if Rails.env.production? || Rails.env.staging?
   Datadog.configure do |c|
     c.env = Rails.env
     c.service = 'moth.social'
+    c.tracing.instrument :httprb, service_name: 'moth.social'
+    c.tracing.instrument :faraday, service_name: 'moth.social'
+    c.tracing.instrument :active_support, service_name: 'moth.social'
+    c.tracing.instrument :http, service_name: 'moth.social'
   end
 end
