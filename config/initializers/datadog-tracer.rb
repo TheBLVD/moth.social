220309b5a4742135ef11aa0ca3ed9ae1215bb5a8
S Dubinsky <1735091+sdubinsky@users.noreply.github.com>
2023-01-24 16:50:27 +0200

    explictly set datadog service names (#104)
    * explictly set datadog service names
    
    * send datadog staging to staging

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,7 +2,15 @@ require 'ddtrace'
 
 if Rails.env.production? || Rails.env.staging?
   Datadog.configure do |c|
-    c.env = Rails.env
+    if Rails.configuration.hosts[0].include? 'staging'
+      c.env = 'staging'
+    else
+      c.env = Rails.env
+    end
     c.service = 'moth.social'
+    c.tracing.instrument :httprb, service_name: 'moth.social'
+    c.tracing.instrument :faraday, service_name: 'moth.social'
+    c.tracing.instrument :active_support, service_name: 'moth.social'
+    c.tracing.instrument :http, service_name: 'moth.social'
   end
 end
