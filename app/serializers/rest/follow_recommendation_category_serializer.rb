6dea856de2520048a1132393a4c4a61fde5513e4
Shalom Dubinsky <smdubinsky@gmail.com>
2023-01-29 21:22:54 +0200

    revert follow rec summary

--- app/serializers/rest/follow_recommendation_category_serializer.rb
+++ app/serializers/rest/follow_recommendation_category_serializer.rb
@@ -6,15 +6,5 @@ class REST::FollowRecommendationCategorySerializer < ActiveModel::Serializer
 
   attributes :name, :theme_color
 
-  has_many :accounts
-
-  def accounts
-    object.accounts.map do |a|
-      {
-        account: REST::AccountSerializer.new(a[:account])
-                                        .as_json
-                                        .merge(summary: a[:summary]),
-      }
-    end
-  end
+  has_many :accounts, serializer: REST::AccountSerializer
 end
