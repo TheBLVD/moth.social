1b32528d63b6663ff95136618aa2cf45743423f3
S Dubinsky <1735091+sdubinsky@users.noreply.github.com>
2023-01-30 16:23:39 +0200

    second pass at recommendation category workers (#109)

--- app/serializers/rest/follow_recommendation_category_serializer.rb
+++ app/serializers/rest/follow_recommendation_category_serializer.rb
@@ -6,5 +6,14 @@ class REST::FollowRecommendationCategorySerializer < ActiveModel::Serializer
 
   attributes :name, :theme_color
 
-  has_many :accounts, serializer: REST::AccountSerializer
+  has_many :accounts
+
+  def accounts
+    object.accounts.map do |a|
+      REST::AccountSerializer
+        .new(a[:account])
+        .as_json
+        .merge(summary: a[:summary])
+    end
+  end
 end
