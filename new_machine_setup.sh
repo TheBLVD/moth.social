d829792c2206da8dedfa6aa27098e131bd64fe99
Shalom Dubinsky <smdubinsky@gmail.com>
2023-02-27 23:05:17 +0200

    add setup script

---
+++ new_machine_setup.sh
@@ -0,0 +1,21 @@
+if [ "$(uname -s)" == "Darwin" ]; then
+    xcode-select --install
+
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    brew install coreutils curl git openssl@3 readline libyaml gmp
+
+    brew install asdf
+
+    echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+
+    . ${ZDOTDIR:-~}/.zshrc
+
+    asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+    asdf install ruby 3.0.1
+
+    gem install bundler
+
+    bundle
+fi
