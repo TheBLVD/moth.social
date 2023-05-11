f4333549386ebe4816f8502924f76cce50535a05
Jesse Tomchak <jtomchak@gmail.com>
2023-05-11 13:28:52 -0700

    commit gen_suggest_follows script

---
+++ gen_suggested_follows.rb
@@ -0,0 +1,29 @@
+require 'csv'
+require 'yaml'
+
+csv = CSV.new(STDIN, headers: true)
+
+categories = csv.group_by { |row| row['Main Category'] }.map do |category, accounts|
+  {
+    'name' => category,
+    'accounts' => accounts.map do |row|
+      case row[0]
+      when 'Account'
+        {
+          'account' => row['Handle'],
+          'type' => 'account',
+          'summary' => row['Brief Description (3-5 words)'],
+        }
+      when 'Hashtag'
+        {
+          'hashtag' => row['Handle'],
+          'type' => 'hashtag',
+          'bio' => row['Description'],
+          'summary' => row['Brief Description (3-5 words)'],
+        }
+      end
+    end.to_a,
+  }
+end.to_a
+
+puts categories.to_yaml
