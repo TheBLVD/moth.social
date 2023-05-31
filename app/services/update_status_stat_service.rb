4fc89f3d993ca77d6af1da3f4c88a3fa7e643821
Jesse Tomchak <jtomchak@gmail.com>
2023-05-31 13:25:14 -0700

    use uri status id for api call, and status record for insert and update

--- app/services/update_status_stat_service.rb
+++ app/services/update_status_stat_service.rb
@@ -14,17 +14,17 @@ class UpdateStatusStatService < BaseService
   def call(status, _options = {})
     # Fetch status from source
     host = URI.parse(status['uri']).host
-    status_id = status['id']
-    get_status("https://#{host}#{ENDPOINT}#{status_id}")
+    status_id = status['uri'].split('/').last
+    get_status("https://#{host}#{ENDPOINT}#{status_id}", status['id'])
   end
 
-  def get_status(url)
+  def get_status(url, id)
     Request.new(:get, url).perform do |response|
       break if response.code != 200
       body = response.body_with_limit
       status = body_to_json(body)
 
-      updated_status = Status.find(status['id'])
+      updated_status = Status.find(id)
 
       updated_status.status_stat.update(
         replies_count: status['replies_count'],
