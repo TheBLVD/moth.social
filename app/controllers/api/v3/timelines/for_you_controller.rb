acf4b357df899b7ddf0aca239a65fd770ee47bcf
Jesse Tomchak <jtomchak@gmail.com>
2023-06-23 11:16:14 -0700

    remove limits on foryou status v3

--- app/controllers/api/v3/timelines/for_you_controller.rb
+++ app/controllers/api/v3/timelines/for_you_controller.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Api::V3::Timelines::ForYouController < Api::BaseController
-  DEFAULT_STATUSES_LIST_LIMIT = 120
+  DEFAULT_STATUSES_LIST_LIMIT = 40
   FOR_YOU_OWNER_ACCOUNT = ENV['FOR_YOU_OWNER_ACCOUNT'] || 'admin'
   LIST_TITLE = 'For You'
-  MINIMUM_ENGAGMENT_ACTIONS = 2
   before_action :set_list
-  before_action :set_statuses
 
   after_action :insert_pagination_headers, unless: -> { @statuses.empty? }
 
@@ -23,20 +21,11 @@ class Api::V3::Timelines::ForYouController < Api::BaseController
   end
 
   def set_owner
-    Account.local.where(username: FOR_YOU_OWNER_ACCOUNT)
-  end
+    @username, @domain = params['acct'].strip.gsub(/\A@/, '').split('@')
+    account = Account.where(username: @username, domain: @domain)
+    return account unless account.nil?
 
-  # Combine engagment actions. Greater than the min engagement set.
-  # Check status for reblog content or assign original content
-  # Reject statues with a reply_to or poll_id
-  # Return the default limit
-  def set_statuses
-    filtered_statuses = cached_list_statuses.select do |s|
-      status = s.reblog? ? s.reblog : s
-      status_counts = status.reblogs_count + status.replies_count + status.favourites_count
-      status_counts >= MINIMUM_ENGAGMENT_ACTIONS && status.in_reply_to_id.nil? && status.poll_id.nil?
-    end
-    @statuses = filtered_statuses.take(limit_param(DEFAULT_STATUSES_LIST_LIMIT))
+    Account.local.where(username: FOR_YOU_OWNER_ACCOUNT)
   end
 
   def cached_list_statuses
@@ -45,7 +34,7 @@ class Api::V3::Timelines::ForYouController < Api::BaseController
 
   def list_statuses
     list_feed.get(
-      limit_param(1000),
+      limit_param(DEFAULT_STATUSES_LIST_LIMIT),
       params[:max_id],
       params[:since_id],
       params[:min_id]
@@ -69,7 +58,7 @@ class Api::V3::Timelines::ForYouController < Api::BaseController
   end
 
   def prev_path
-    api_v2_timelines_for_you_url pagination_params(min_id: pagination_since_id)
+    api_v3_timelines_for_you_url pagination_params(min_id: pagination_since_id)
   end
 
   def pagination_max_id
