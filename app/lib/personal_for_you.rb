97048ab618646ab22077319abe0cc10ae4ad3972
Jesse Tomchak <jtomchak@gmail.com>
2023-08-17 09:29:45 -0700

    logging

--- app/lib/personal_for_you.rb
+++ app/lib/personal_for_you.rb
@@ -21,6 +21,7 @@ class PersonalForYou
     account_handle = account.local? ? account.local_username_and_domain : account.acct
     cache_key = "follow_recommendations:#{account_handle}"
     fedi_account_handles = Rails.cache.fetch(cache_key)
+    Rails.logger.info "INDIRECT FOLLOW RECOMMENDATIONS \n #{fedi_account_handles}"
     # Parse handles into username & domain array for batch account query
     username_query = Array.[]
     domain_query = Array.[]
@@ -31,6 +32,8 @@ class PersonalForYou
     end
     # Array of account id's from fedi_account_handles
     account_ids = Account.where(username: username_query, domain: domain_query).pluck(:id)
+    Rails.logger.info "INDIRECT FOLLOW RECOMMENDATIONS \n #{username_query}"
+    Rails.logger.info "INDIRECT FOLLOW RECOMMENDATIONS \n #{account_ids}"
     # Get Statuses for those accounts
     Status.where(account_id: account_ids, updated_at: 12.hours.ago..Time.now).limit(200)
   end
@@ -65,16 +68,19 @@ class PersonalForYou
 
   # Get Mammoth user following
   def user_following(acct)
-    response = HTTP.headers({ Authorization: ACCOUNT_RELAY_AUTH, 'Content-Type': 'application/json' }).get(
-      "https://#{ACCOUNT_RELAY_HOST}/api/v1/foryou/users/#{acct}/following"
-    )
-    # Get Following
-    results = JSON.parse(response.body)['following']
-    return results unless response.code != 200
+    Async do
+      response = HTTP.headers({ Authorization: ACCOUNT_RELAY_AUTH, 'Content-Type': 'application/json' }).get(
+        "https://#{ACCOUNT_RELAY_HOST}/api/v1/foryou/users/#{acct}/following"
+      )
+      # Get Following
+      results = JSON.parse(response.body)['following']
+      return results unless response.code != 200
+    end
   end
 
   def statuses_for_direct_follows(acct)
-    following = user_following(acct)
+    following = user_following(acct).await
+    Rails.logger.info "FOLLOW RECOMMENDATIONS \n #{fedi_account_handles}"
     # Parse handles into username & domain array for batch account query
     username_query = Array.[]
     domain_query = Array.[]
@@ -84,6 +90,8 @@ class PersonalForYou
       username_query.push(user['username'])
       domain_query.push(domain)
     end
+    Rails.logger.info "FOLLOW RECOMMENDATIONS \n #{username_query}"
+    Rails.logger.info "FOLLOW RECOMMENDATIONS \n #{domain_query}"
     # Array of account id's from fedi_account_handles
     account_ids = Account.where(username: username_query, domain: domain_query).pluck(:id)
     # Get Statuses for those accounts
