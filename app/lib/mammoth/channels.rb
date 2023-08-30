c600099eb2c611e3e4fed5a7fe83658fe1a4d42e
Jesse Tomchak <jtomchak@gmail.com>
2023-08-30 08:10:48 -0700

    MAM-2493-core-channel-feed (#173)
    * channels api calls
    * channel details api calls

---
+++ app/lib/mammoth/channels.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+module Mammoth
+  class Channels
+    ACCOUNT_RELAY_AUTH = "Bearer #{ENV.fetch('ACCOUNT_RELAY_KEY')}"
+    ACCOUNT_RELAY_HOST = 'acctrelay.moth.social'
+
+    # GET all available channels as a list
+    # Channl includes id, title, description, owner
+    def list
+      cache_key = 'channels:list'
+      Rails.cache.fetch(cache_key, expires_in: 60.seconds) do
+        response = HTTP.headers({ Authorization: ACCOUNT_RELAY_AUTH, 'Content-Type': 'application/json' }).get(
+          "https://#{ACCOUNT_RELAY_HOST}/api/v1/channels"
+        )
+        JSON.parse(response.body, symbolize_names: true)
+      end
+    end
+
+    def find(id)
+      cache_key = "channels:list:#{id}"
+      Rails.cache.fetch(cache_key, expires_in: 60.seconds) do
+        response = HTTP.headers({ Authorization: ACCOUNT_RELAY_AUTH, 'Content-Type': 'application/json' }).get(
+          "https://#{ACCOUNT_RELAY_HOST}/api/v1/channels/#{id}"
+        )
+        JSON.parse(response.body, symbolize_names: true)
+      end
+    end
+  end
+end
