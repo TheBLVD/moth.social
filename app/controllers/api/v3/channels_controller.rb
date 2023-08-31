833432beece096de8905a94cdcd54c56773e9385
Jesse Tomchak <jtomchak@gmail.com>
2023-08-31 11:29:29 -0700

    rethrow 404 from acctrelay (#174)

--- app/controllers/api/v3/channels_controller.rb
+++ app/controllers/api/v3/channels_controller.rb
@@ -9,9 +9,15 @@ class Api::V3::ChannelsController < Api::BaseController
 
   def show
     @mammoth = Mammoth::Channels.new
-    @channel = @mammoth.find(channel_id_param)
-    render json: @channel
+    channel = @mammoth.find(channel_id_param)
+    render json: channel
   end
+
+  rescue_from Mammoth::Channels::NotFound do |e|
+    render json: { error: e.to_s }, status: 404
+  end
+
+  private
 
   def channel_id_param
     params.require(:id)
