df613ec669d01d03bcfb496033306c8d6ff9c20d
Jesse Tomchak <jtomchak@gmail.com>
2023-08-31 10:55:44 -0700

    remove beta list references, use mammoth_user? instead

--- app/workers/scheduler/status_stat_update_scheduler.rb
+++ app/workers/scheduler/status_stat_update_scheduler.rb
@@ -67,8 +67,8 @@ class Scheduler::StatusStatUpdateScheduler
   # Take the accounts from the beta list, get all the indirect follows
   def statuses_from_personalized_for_you
     personal_for_you = PersonalForYou.new
-    personal_for_you.beta_list_accounts
-                    .map { |account| personal_for_you.statuses_for_indirect_follows(account) }
+    personal_for_you.mammoth_users
+                    .map { |acct| personal_for_you.statuses_for_indirect_follows(acct) }
                     .flatten
   end
 
