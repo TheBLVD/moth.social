0a33d5b8b08ff119ddfad59f1a338ddc978a1cf8
Jesse Tomchak <jtomchak@gmail.com>
2023-09-13 10:58:59 -0700

    MAM-2599-api-endpoints-for-each-channel-feed-by-channel-id (#180)
    * Return Channel Statuses by channel id
    * Return with pagination, like a list

---
+++ app/controllers/api/v3/timelines/channels_controller.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+class Api::V3::Timelines::ChannelsController < Api::BaseController
+  before_action :set_channel
+
+  after_action :insert_pagination_headers, only: [:show], unless: -> { @statuses.empty? }
+
+  rescue_from Mammoth::Channels::NotFound do |e|
+    render json: { error: e.to_s }, status: 404
+  end
+
+  def show
+    @statuses = cached_channel_statuses
+    render json: @statuses,
+           each_serializer: REST::StatusSerializer,
+           relationships: StatusRelationshipsPresenter.new(@statuses, current_user&.account_id)
+  end
+
+  private
+
+  def set_channel
+    @mammoth = Mammoth::Channels.new
+    @channel = @mammoth.find(channel_id_param)
+  end
+
+  def cached_channel_statuses
+    cache_collection channel_statuses, Status
+  end
+
+  # LIST_LIMT set in Api::BaseController
+  def channel_statuses
+    channel_feed.get(DEFAULT_STATUSES_LIST_LIMIT,
+                     params[:max_id],
+                     params[:since_id],
+                     params[:min_id])
+  end
+
+  def channel_feed
+    MammothChannelFeed.new(@channel)
+  end
+
+  def channel_id_param
+    params.require(:id)
+  end
+
+  # PAGINATION
+  def insert_pagination_headers
+    set_pagination_headers(next_path, prev_path)
+  end
+
+  def pagination_params(core_params)
+    params.slice(:limit).permit(:limit).merge(core_params)
+  end
+
+  def next_path
+    api_v3_timelines_channel_url pagination_params(max_id: pagination_max_id)
+  end
+
+  def prev_path
+    api_v3_timelines_channel_url pagination_params(min_id: pagination_since_id)
+  end
+
+  def pagination_max_id
+    @statuses.last.id
+  end
+
+  def pagination_since_id
+    @statuses.first.id
+  end
+end
