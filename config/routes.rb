ac027b46d5380890e2c3415069083a2a3ef80ec6
Jesse Tomchak <jtomchak@gmail.com>
2023-09-13 08:58:16 -0700

    pagination url reference

--- config/routes.rb
+++ config/routes.rb
@@ -723,7 +723,7 @@ Rails.application.routes.draw do
       end 
       
       namespace :timelines do
-        get 'channels/:id', to: 'channels#show'
+        resources :channels, only: :show, controller: :channels
         resource :for_you, only: [:show], controller: 'for_you' do
           get '/me',      to: 'for_you#index'
           put '/me',      to: 'for_you#update'
