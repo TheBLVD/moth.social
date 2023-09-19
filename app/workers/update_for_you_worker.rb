fb0ed0fbcd6dbc65a6d4b182cc73956143f9dbfe
Jesse Tomchak <jtomchak@gmail.com>
2023-09-19 08:36:46 -0700

    MAM-2694-show-the-mammoth-picks-switch-in-the-for-you-customization-view (#182)
    ForYou now adds Mammoth Curated List when settings toggle is enabled.

--- app/workers/update_for_you_worker.rb
+++ app/workers/update_for_you_worker.rb
@@ -2,10 +2,11 @@
 
 class UpdateForYouWorker
   include Redisable
-  include Async
   include Sidekiq::Worker
 
   sidekiq_options retry: 0, queue: 'pull'
+
+  # Mammoth Curated List(OG List)
 
   #  Fetch Acct Config from AcctRelay
   #  Fetch Following from AcctRelay
@@ -45,7 +46,8 @@ class UpdateForYouWorker
     push_following_status
     # Channel Feed
     push_channels_status
-    # Public Feed
+    # Mammoth Curated OG Feed
+    push_mammoth_curated_status
   end
 
   def update_user_status(status)
@@ -86,7 +88,7 @@ class UpdateForYouWorker
              .each { |s| ForYouFeedWorker.perform_async(s['id'], @account.id, 'personal') }
   end
 
-  # Channels Subscribed too
+  # Channels Subscribed
   def push_channels_status
     user_setting = @user[:for_you_settings]
     return if user_setting[:from_your_channels].zero?
@@ -94,6 +96,18 @@ class UpdateForYouWorker
     @personal.statuses_for_subscribed_channels(@user)
              .filter_map { |s| engagment_threshold(s, user_setting[:from_your_channels], 'channel') }
              .each { |s| ForYouFeedWorker.perform_async(s['id'], @account.id, 'personal') }
+  end
+
+  # Mammoth Curated OG List
+  def push_mammoth_curated_status
+    user_setting = @user[:for_you_settings]
+    return if user_setting[:curated_by_mammoth].zero?
+
+    curated_list = Mammoth::CuratedList.new
+    list_statuses = curated_list.curated_list_statuses
+
+    list_statuses.filter_map { |s| engagment_threshold(s, user_setting[:curated_by_mammoth], 'mammoth') }
+                 .each { |s| ForYouFeedWorker.perform_async(s['id'], @account.id, 'personal') }
   end
 
   # Check status for User's level of engagment
@@ -111,11 +125,11 @@ class UpdateForYouWorker
   # 1,2,3 relates to low,med, high and it's respectice value as it relates to engagment
   def engagment_metrics(type)
     case type
-    when 'following'
+    when 'following', 'mammoth'
       { 1 => 2, 2 => 4, 3 => 6 }
     when 'channel'
       { 1 => 0, 2 => 1, 3 => 2 }
-    when 'indirect', 'public'
+    when 'indirect'
       { 1 => 1, 2 => 2, 3 => 3 }
     end
   end
