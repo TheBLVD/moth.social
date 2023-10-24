5b12b425ae74b1669da7f586b8ea25723bc73374
Jesse Tomchak <jtomchak@gmail.com>
2023-10-24 16:56:11 -0700

    the right channel id

--- app/lib/mammoth/status_origin.rb
+++ app/lib/mammoth/status_origin.rb
@@ -16,16 +16,28 @@ module Mammoth
         reason = channel_reason(status, channel)
         
         # Expire Reason in 7 days
-        redis.sadd(list_key, reason)
-        redis.expire(list_key, 7.day.seconds) 
+        add_reason(list_key, reason)
     end
+
+    def add_mammoth_pick(status)
+        list_key = key(status[:id])
+        reason = mammoth_pick_reason(status)
+
+        add_reason(list_key, reason)
+    end 
+
+    def add_reason(key, reason)
+        # Expire Reason in 7 days
+        redis.sadd(key, reason)
+        redis.expire(key, 7.day.seconds) 
+    end 
 
     def find(status_id)
         list_key = key(status_id)
         results = redis.smembers(list_key).map { |o| 
             payload = Oj.load(o, symbol_keys: true)
             originating_account = Account.create(payload[:originating_account])
-            origin = ::StatusOrigin.new(source: payload[:source], channel_id: payload[:id], title: payload[:title], originating_account:originating_account )
+            origin = ::StatusOrigin.new(source: payload[:source], channel_id: payload[:channel_id], title: payload[:title], originating_account:originating_account )
             Rails.logger.debug "AR:: ACCOUNT  #{originating_account}"
             Rails.logger.debug "AR:: ORIGIN  #{origin}"
             origin
@@ -48,6 +60,10 @@ module Mammoth
 
     def channel_reason(status, channel)
         Oj.dump({source: "SmartList", channel_id: channel[:id], title: channel[:title], originating_account: status.account})
+    end
+
+    def mammoth_pick_reason(status)
+        Oj.dump({source: "MammothPick", originating_account: status.account })
     end
   end
 end
