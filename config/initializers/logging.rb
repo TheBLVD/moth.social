a0f51da2b6f6f216ea47dce8108d16eaaf6d32c3
Jesse Tomchak <jtomchak@gmail.com>
2023-11-07 12:32:06 -0700

    logging

---
+++ config/initializers/logging.rb
@@ -0,0 +1,3 @@
+console_logger = ActiveSupport::Logger.new(STDOUT)
+appsignal_logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new('rails'))
+Rails.logger = console_logger.extend(ActiveSupport::Logger.broadcast(appsignal_logger))
