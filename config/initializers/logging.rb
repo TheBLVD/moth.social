7dc58dc6b256af9a4ae0f2bf171e47e0f49dc10d
Jesse Tomchak <jtomchak@gmail.com>
2023-11-08 09:03:00 -0700

    working_directory

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+
 console_logger = ActiveSupport::Logger.new(STDOUT)
 appsignal_logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new('rails'))
 Rails.logger = console_logger.extend(ActiveSupport::Logger.broadcast(appsignal_logger))
