d02136ea33d998ce36bac021f4fab92e739408be
Jesse Tomchak <jtomchak@gmail.com>
2023-11-15 09:03:50 -0700

    wrap mammoth picks

--- app/lib/mammoth/curated_list.rb
+++ app/lib/mammoth/curated_list.rb
@@ -9,19 +9,25 @@ module Mammoth
     LIST_TITLE = 'For You'
 
     def curated_list_statuses
-      account_ids = mammoth_curated_accounts
+      cache_key = 'mammoth_picks:accounts'
+      account_ids = Rails.cache.fetch(cache_key, expires_in: 1.day) do
+        mammoth_curated_accounts
+      end
       statuses_from_list(account_ids)
+    end
+
+    def statuses_from_list(account_ids)
+      cache_key = 'mammoth_picks:statuses'
+      Rails.cache.fetch(cache_key, expires_in: 60.seconds) do
+        Status.where(account_id: account_ids,
+                     created_at: (GO_BACK.hours.ago)..Time.current)
+      end
     end
 
     def mammoth_curated_accounts
       owner_account = Account.local.where(username: FOR_YOU_OWNER_ACCOUNT)
       @list = List.where(account: owner_account, title: LIST_TITLE).first!
       @list.list_accounts.pluck(:account_id)
-    end
-
-    def statuses_from_list(account_ids)
-      Status.where(account_id: account_ids,
-                   created_at: (GO_BACK.hours.ago)..Time.current)
     end
   end
 end
