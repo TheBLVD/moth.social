c6b78a17ebe2916211433ce70c285fdb339f7407
Jesse Tomchak <jtomchak@gmail.com>
2023-11-20 14:50:44 -0700

    return early from update scheduler if processes are already running

---
+++ app/services/for_you_mammoth_service.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Checks to see if UpdateForYouWokers are still running jobs from last
+# scheduled batch.
+class ForYouMammothService < BaseService
+  def call
+    update_worker_in_process?
+  end
+
+  # Check specificly for any UpdateWorker
+  # in the 'mammoth_default' queue
+  # rebuilds are sent to 'mammoth_critical'
+  def update_worker_in_process?
+    queue = Sidekiq::Queue.new('mammoth_default')
+
+    queue.any? do |job|
+      job.klass == UpdateForYouWorker.to_s
+    end
+  end
+end
