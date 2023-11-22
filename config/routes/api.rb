99d64ab1f527359352cf30c5412fdf210af4061a
Jesse Tomchak <jtomchak@gmail.com>
2023-11-22 09:48:31 -0700

    statuses too

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -335,6 +335,7 @@ namespace :api, format: false do
   namespace :v4 do
     namespace :timelines do
       resource :for_you, only: [:show], controller: 'for_you' do
+        resources :statuses, only: :show, controller: :statuses
         get '/me',      to: 'for_you#index'
         put '/me',      to: 'for_you#update'
       end
